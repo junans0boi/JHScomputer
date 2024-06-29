45451a4e346e6ba61d0c7f2556bbf235b31c872a
junans0boi <105141262+junans0boi@users.noreply.github.com>
2024-06-29 22:51:49 +0900

    server init

---
+++ Server/src/main/java/com/jhscomputer/jhscomputer/JhscomputerApplication.java
@@ -0,0 +1,13 @@
+package com.jhscomputer.jhscomputer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhscomputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhscomputerApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/jhscomputer/jhscomputer/JhscomputerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhscomputer.jhscomputer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhscomputerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
